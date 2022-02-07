927ecb2b58be5a44a15040a894460e221699a5f2
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-02-07 18:31:18 -0300

    m

---
+++ scripts/login.js
@@ -0,0 +1,40 @@
+let username = "";
+let password = "";
+
+function CreateCookie (userhash){
+    // Cria uma nova data no futuro 01/01/2020
+    var date = new Date();
+    date.setTime(date.getTime() + (30*24*60*60*1000));
+    // Converte a data para GMT
+    // Wed, 01 Jan 2020 02:00:00 GMT
+    date = date.toGMTString();
+    // Cria o cookie
+    document.cookie = `userhash=${userhash}; expires=${date};`;
+}
+
+function Login () 
+{
+    const user = {"user":username, "pass":password};
+    fetch("http://vacsina.servegame.com:8000/user", 
+    {
+        method: 'post',
+        mode: 'cors',
+        body: JSON.stringify(user),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then((resp) => resp.json())
+    .then(function (data) {
+        //console.log('Request succeeded with JSON response', data);
+        console.log(data);
+        //CreateCookie(data);
+    })
+    .catch(function (error) {
+        console.log('Request failed', error);
+    });
+}
+
+$("#username").on("input", (e) => {username = e.target.value;});
+$("#password").on("input", (e) => {password = e.target.value;});
+//username = 
+
+//Login ();
